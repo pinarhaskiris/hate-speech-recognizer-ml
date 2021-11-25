24d34241a801b85f402812754a2c26a4e384e94e
pinarhaskiris <pinarhaskiris1900@gmail.com>
2021-11-25 15:50:19 +0300

    cleaned

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
 import re
+import string
 
-hateful = []
-not_hateful = []
 
 # open file for reading
 with open('gab.csv') as dataset:
@@ -17,15 +16,38 @@ with open('gab.csv') as dataset:
         if (match):
             conversations.append(row)
 
-#collect posts and indexes of hateful ones for each conversation
-conversation_parts = []
-for i in range(len(conversations)):
-    part = (conversations[i][1], conversations[i][2])
-    conversation_parts.append(part)
+non_hateful = open("non_hateful.txt", "a")
+hateful = open("hateful.txt", "a")
 
-str1 = str(conversations[0][1].split('\n')).replace(r'\t', '')
-print(str1)
+#selected a conversation for testing
+for i in range(len(conversations)): #each conversation
+    for convo in conversations[i][1].split('\n'): #each post in the conversation
+        convo = re.sub(r'http\S+', '', convo) #remove links
+        base = []
+        for j in range(len(convo)): #for each char in a post
+            if (convo[j].isalnum() or (convo[j].isspace()) or (convo[j] in string.punctuation) or convo[j] == "’" or convo[j] == "‘"):
+                base.append(convo[j])
+                base_joined = ''.join(base) #combine chars
+                clear_withs  = base_joined.replace("w/", "with") #fix withs
+                post_index = clear_withs[0]
+                is_written = False
+                no_index = clear_withs.lstrip('0123456789.') #remove the index numbers of the posts
+                no_tags = re.sub('(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9-_]+)', '', no_index)
+                no_mentions = re.sub('(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)', '', no_tags)
+                single_punctuaions = re.sub(r'[\?\.\!]+(?=[\?\.\!])', '', no_mentions)
 
+                abrevs = re.findall(r"\b[A-Z]{2,}\b", single_punctuaions)
+                for abrev in abrevs:
+                    single_punctuaions = single_punctuaions.replace(abrev, abrev.title())
+
+        if (post_index in conversations[i][2] and not is_written):
+            hateful.write(f"{single_punctuaions} \n")
+            is_written = True
+        else:
+            non_hateful.write(f"{single_punctuaions} \n")
+
+non_hateful.close()
+hateful.close()
 
 
 
